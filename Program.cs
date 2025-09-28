dd6decd100574ca8d2e0020e96ba09e1c415b36f
Iryna <kytsenkoiryna33@gmail.com>
2025-09-28 19:44:32 +0300

    LSP

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+abstract class Vehicle
+{
+    public abstract void Move();
+}
+
+class Car : Vehicle
+{
+    public override void Move() => Console.WriteLine("Car is driving on the road");
+}
+
+class Bicycle : Vehicle
+{
+    public override void Move() => Console.WriteLine("Bicycle is pedaling forward");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle v1 = new Car();
+        Vehicle v2 = new Bicycle();
+
+        v1.Move();
+        v2.Move();
+    }
+}
+
+
